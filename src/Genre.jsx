f3ff129b5b507ecd0635836759e798ec7ee3024d
collarish <118124469+collarish@users.noreply.github.com>
2023-03-10 15:17:07 +0100

    linked genres

---
+++ src/Genre.jsx
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+
+
+
+export const Genre = ({ id }) => {
+    const [genres, setGenres] = useState([]);
+
+    const fetchData = async () => {
+        const response =
+            await fetch(`https://musicbrainz.org/ws/2/artist/${id}?inc=genres&fmt=json`);
+        const data = await response.json();
+        console.log(data);
+        setGenres(data.genres);
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    return (
+        <div>
+            <div className="genre-container">{genres.map((genre) => {
+                return <div className="genre">{genre.name}</div>
+            })}
+            </div>
+        </div>
+    )
+}
+
+export default Genre
