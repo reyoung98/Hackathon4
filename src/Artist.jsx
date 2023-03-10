d6b51d48d3abf238495c099895e37491b95175ea
Rachel Young <reyoung255@gmail.com>
2023-03-10 12:24:54 +0100

    some more styles

--- src/Artist.jsx
+++ src/Artist.jsx
@@ -8,9 +8,9 @@ export default function Artist({ artists }) {
                     <a href={`/artist/${artist.id}`} className="artist-card" key={artist.id}>
                         {/* <div className="avatar"></div> */}
                         <div className="artist-name">{artist.name}</div>
-                        <div className="artist-country">{artist.country}</div>
+                        {/* <div className="artist-country">{artist.country}</div> */}
                         <div className="artist-disambiguation">{artist.disambiguation}</div>
-                        {/* <div className="artist-name">{artist.tags[0].name}</div> */}
+                        <div className="artist-aliases">Aliases: {artist.aliases.map(alias => alias.name).join(", ")}</div>
                     </a>
                 )
             }
